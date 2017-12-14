cbf370fe4eb367c22665bb887df01c498731faed
Marc Geurts <geurtsmarc@hotmail.com>
2017-12-14 15:15:43 +0100

    use class

--- server.js
+++ server.js
@@ -1,71 +1,93 @@
 'use strict';
 
 const bodyParser = require('body-parser');
+const socket = require('socket.io');
 const express = require('express');
-const https = require('https');
-const http = require('http');
+const corser = require('cors');
 const path = require('path');
-const cors = require('cors');
-const fs = require('fs');
-const app = express();
-const port = 8000;
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(require('helmet')());
-app.use(cors({origin: '*'}));
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', false);
-    next();
-});
-// const options = {
-//     key: fs.readFileSync('privkey.pem'),
-//     cert: fs.readFileSync('fullchain.pem'),
-//     requestCert: false,
-//     rejectUnauthorized: false
-// };
+class Server{
 
-const io = require('socket.io').listen(app.listen(port), {
-    log: false,
-    agent: false,
-    origins: '*:*',
-    transports: ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling', 'polling']
-});
+    constructor(){
+        this.port =  process.env.PORT || 8000;
+        this.app = express();
+        this.io = socket.listen(this.app.listen(this.port), {
+            log: false,
+            agent: false,
+            origins: '*:*',
+            transports: ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling', 'polling']
+        });
+    }
 
-// http.createServer(server).listen(8000);
-// https.createServer(options, server).listen(8443);
+    config(){
+        this.app.use(
+            bodyParser.urlencoded({
+                extended: true
+            })
+        );
 
-io.on('connection', (socket) => {
-    console.log('user connected');
+        this.app.use(
+            bodyParser.json()
+        );
 
-    socket.on('add-message', (message) => {
-        io.emit('message', message);
-    });
+        this.app.use(
+            require('helmet')()
+        );
 
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-});
+        this.app.use(
+            corser({
+                origin: '*'
+            })
+        );
 
+        this.app.use(function (req, res, next) {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+            res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+            res.setHeader('Access-Control-Allow-Credentials', false);
+            next();
+        });
+    }
 
-io.sockets.on("connection",function(socket){
-    socket.emit("start_server");
-})
+    routes(){
+        this.app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, 'index.html'));
+        });
+    }
 
-io.sockets.on("disconnect",function(socket){
-    console.log('user disconnected (2)');
-});
+    execute(){
+        this.config();
+        this.routes();
 
-// io.sockets.emit("EVENT_NAME",EVENT_DATA);
-// //EVENT_DATA Can Be Anything That Is To Be Sent To The Server
+        this.io.sockets.on("connection",function(socket){
+            socket.emit("start_server");
+        });
 
-// io.sockets.on("EVENT_NAME",function(data){
-// //data = EVENT_DATA Passed From The emit Method
-// })
+        this.io.on('connection', (socket) => {
+            console.log('user connected');
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+            socket.on('add-message', (message) => {
+                io.emit('message', message);
+            });
+
+            socket.on('disconnect', function () {
+                console.log('user disconnected');
+            });
+        });
+
+        this.io.sockets.on("disconnect",function(socket){
+            console.log('user disconnected (2)');
+        });
+
+        // this.io.sockets.emit("EVENT_NAME",EVENT_DATA);
+        // //EVENT_DATA Can Be Anything That Is To Be Sent To The Server
+
+        // this.io.sockets.on("EVENT_NAME",function(data){
+        // //data = EVENT_DATA Passed From The emit Method
+        // })
+    }
+
+}
+
+const app = new Server();
+app.execute();
